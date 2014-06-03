4e72829c9dc0ea946d13c0288dd598aad3244c3e
Alex Deaconu <alex.deaconu@risevision.com>
2014-06-03 13:45:40 -0400

    Panel should not be shown on init
    removed old comments
    
    removed file url retrieval methods

--- src/com/risevision/ui/client/common/widgets/iframe/IFramePanelWidget.java
+++ src/com/risevision/ui/client/common/widgets/iframe/IFramePanelWidget.java
@@ -67,7 +67,6 @@ public abstract class IFramePanelWidget extends PopupPanel {
 				
 		HtmlUtils.writeHtml(iFrameElement.getElement(), htmlString);
 		
-		showPanel(true);
 	}
 
 	protected void onLoad() {
