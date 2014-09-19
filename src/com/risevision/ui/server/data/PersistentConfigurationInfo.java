1ead7816c259d3d6d533801c21429f53593106de
Steven Sherrie <steve.sherrie@risevision.com>
2014-09-19 18:25:00 -0400

    Update dev and production store hosts (ssl in production)

--- src/com/risevision/ui/server/data/PersistentConfigurationInfo.java
+++ src/com/risevision/ui/server/data/PersistentConfigurationInfo.java
@@ -66,7 +66,8 @@ public class PersistentConfigurationInfo implements Serializable {
     public static final String FINANCIAL_SERVER_URL = "http://contentfinancial2-test.appspot.com";
 //    public static final String FINANCIAL_SERVER_URL = "http://contentfinancial2.appspot.com";
 
-    private static final String STORE_URL = "http://192.254.220.35/store/";
+    private static final String STORE_URL = "http://192.254.220.35/store-ci/"; // Dev
+//    private static final String STORE_URL = "https://store.risevision.com/"; // Production
 	
 	@Persistent
 	@PrimaryKey
